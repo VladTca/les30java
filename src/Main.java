33512a3e9f06d1788073d3ecfbdfd0b23cad73c6
VladTca <votsats@gmail.com>
2025-02-10 23:50:33 +0100

    Replace forEach with ListIterator for flybles in main method

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ListIterator;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,6 +12,10 @@ public class Main {
         flybles.add(new Bird(4, 5, 6));
 
 
-       flybles.forEach(Flyble::fly);
+//       flybles.forEach(Flyble::fly);
+        ListIterator <Flyble> ls = flybles.listIterator();
+        while (ls.hasNext()) {
+            ls.next().fly();
+        }
     }
 }
